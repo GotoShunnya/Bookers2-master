70dc61f7327f0164b073982f3ffa44cafe0719c9
Gotoshunya <1800syunya@Gmail.com>
2021-05-04 10:39:01 +0000

    5/4 エラー文（カラム）日本語化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,8 @@ class BookCommentsController < ApplicationController
     @comment = BookComment.new(book_comment_params)
     @comment.user_id = current_user.id
     @comment.book_id = @book.id
-    if @comment.save
-    else #エラーメッセージ表示できん
-      @book1 = Book.new
-      @user = @book.user
-      render "books/show"
-    end
+    @comment.save
+    #エラーメッセージ表示できん
   end
 
   def destroy
